e8e207e5af12515b70e605743467349b4594b16a
Duth Ng <rumduth1@gmail.com>
2025-11-06 01:43:59 -0600

    Holy Grail

---
+++ React/Holy Grail/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <header>Header</header>
+      <div className="content">
+        <nav>Navigation</nav>
+        <main>Main</main>
+        <aside>Sidebar</aside>
+      </div>
+      <footer>Footer</footer>
+    </div>
+  );
+}
+
+export default App;
